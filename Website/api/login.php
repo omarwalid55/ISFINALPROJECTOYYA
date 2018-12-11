ca5c4dd23d927863747a540d8b743138af573552
omarwalid55 <36336200+omarwalid55@users.noreply.github.com>
2018-12-11 20:57:49 +0200

    sign still needs improvment

---
+++ Website/api/login.php
@@ -0,0 +1,72 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/Website/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// files needed to connect to database
+include_once 'config/database.php';
+include_once 'objects/users.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// instantiate user object
+$user = new user($db);
+ 
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// set product property values
+$user->username = $data->username;
+$username_exists = $user->usernameExists();
+ 
+// generate json web token
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+
+// check if username exists and if password is correct
+if($username_exists && password_verify($data->password, $user->password)){
+ 
+    $token = array(
+       "iss" => $iss,
+       "aud" => $aud,
+       "iat" => $iat,
+       "nbf" => $nbf,
+       "data" => array(
+           "name" => $user->name,
+           "username" => $user->username,
+           
+       )
+    );
+ 
+    // set response code
+    http_response_code(200);
+ 
+    // generate jwt
+    $jwt = JWT::encode($token, $key);
+    echo json_encode(
+            array(
+                "message" => "Successful login.",
+                "jwt" => $jwt
+            )
+        );
+ 
+}
+// login failed
+else{
+ 
+    // set response code
+    http_response_code(401);
+ 
+    // tell the user login failed
+    echo json_encode(array("message" => "Login failed."));
+}
+?>
---
+++ Website/api/objects/users.php
@@ -0,0 +1,107 @@
+<?php
+// 'user' object
+class user{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+ 
+    // object properties
+    public $name;
+    public $email;
+    public $username;
+    public $password;
+    public $type;
+
+
+    // constructor
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ 
+// create new user record
+function create(){
+ 
+    // insert query
+    $query = "INSERT INTO " . $this->table_name . "
+            SET
+                name = :name,
+                email = :email,
+                username = :username,
+                password = :password,
+                type = :type";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->email=htmlspecialchars(strip_tags($this->email));
+    $this->username=htmlspecialchars(strip_tags($this->username));
+    $this->password=htmlspecialchars(strip_tags($this->password));
+    $this->type=htmlspecialchars(strip_tags($this->type));
+ 
+    // bind the values
+    $stmt->bindParam(':name', $this->name);
+    $stmt->bindParam(':email', $this->email);
+    $stmt->bindParam(':username', $this->username);
+    $stmt->bindParam(':type', $this->type);
+
+ 
+    // hash the password before saving to database
+    $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password_hash);
+ 
+    // execute the query, also check if query was successful
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+// check if given email exist in the database
+function emailExists(){
+ 
+    // query to check if email exists
+    $query = "SELECT name, username, type, password
+            FROM " . $this->table_name . "
+            WHERE email = ?
+            LIMIT 0,1";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare( $query );
+ 
+    // sanitize
+    $this->email=htmlspecialchars(strip_tags($this->email));
+ 
+    // bind given email value
+    $stmt->bindParam(1, $this->email);
+ 
+    // execute the query
+    $stmt->execute();
+ 
+    // get number of rows
+    $num = $stmt->rowCount();
+ 
+    // if email exists, assign values to object properties for easy access and use for php sessions
+    if($num>0){
+ 
+        // get record details / values
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        // assign values to object properties
+        $this->name = $row['name'];
+        $this->username = $row['username'];
+        $this->password = $row['password'];
+        $this->type = $row['type'];
+ 
+        // return true because email exists in the database
+        return true;
+    }
+ 
+    // return false if email does not exist in the database
+    return false;
+}
+
+}
+ 
