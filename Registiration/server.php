be0f9053269c596a15481882029c1c674376775d
yousseffkhaled <36365406+yousseffkhaled@users.noreply.github.com>
2018-12-14 19:30:35 +0200

    access control of register

--- Registiration/server.php
+++ Registiration/server.php
@@ -58,8 +58,13 @@ if (isset($_POST['reg_user'])) {
   			  VALUES('$name','$username', '$email', '$password','$type')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+    $_SESSION['success'] = "You are now logged in";
+    if ($type === "Student"){
+    header('location: student.php');}
+   else if ($type === "Teacher Assistant"){
+      header('location: ta.php');}
+     else if ($type === "Lecturer"){
+        header('location: lecturer.php');}
   }
 }
 // LOGIN USER
@@ -75,13 +80,14 @@ if (isset($_POST['login_user'])) {
     }
   
     if (count($errors) == 0) {
-        $password = md5($password);
+      $password = md5($password);
         $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
           $_SESSION['success'] = "You are now logged in";
-          header('location: index.php');
+          header('location: student.php');
         }else {
             array_push($errors, "Wrong username/password combination");
         }
